04736521ff83a9452bbd066ab1c7682a7323fcff
unknown <sidhemail@gmail.com>
2024-03-03 16:27:23 +0900

    added minimal code to check how it works.

--- bin/cdk-basics.ts
+++ bin/cdk-basics.ts
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkBasicsStack } from '../lib/cdk-basics-stack';
+import { VpcNetworkStack } from '../lib/stacks/vpc-network-stack';
+import { CdkUtil } from '../utils/cdk-util';
+
+const cdkUtil = CdkUtil.getInstance();
+
+console.log(cdkUtil.e.environmentPassingInStack);
 
 const app = new cdk.App();
-new CdkBasicsStack(app, 'CdkBasicsStack');
+const vpcNetworkStackName = cdkUtil.naming.generateResourceName('vpc');
+console.log ("VPC Stack Name: " + vpcNetworkStackName)
+
+new VpcNetworkStack(app, vpcNetworkStackName, {env: cdkUtil.e.environmentPassingInStack});
+
+
